198abecd6ce9a31a45bc228f679d7b56d86dc1dd
tahoito <taho.ito0304@gmail.com>
2025-12-15 12:16:44 +0900

    welcomeページ追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('welcome');
 
 Route::get('/test', fn () => view('pages.test'));
 
