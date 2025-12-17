c4bb1de346ec65a8ceb71cad0bea9dd3c9bc4c91
tahoito <taho.ito0304@gmail.com>
2025-12-18 07:43:08 +0900

    アイコンと検索バー

--- src/resources/views/pages/test.blade.php
+++ src/resources/views/pages/test.blade.php
@@ -5,7 +5,11 @@
     <x-ui.button theme="store" class="w-[90%] mx-auto">サインアップ</x-ui.button>
     <x-ui.button variant="secondary" class="w-[70%] mx-auto">次へ</x-ui.button>
     <x-ui.button variant="next">戻る</x-ui.button>
-    <x-icons.user class="w-8 h-8 text-main"/>
+    <x-icons.store class="w-8 h-8 text-main"/>
     <x-ui.input type="text" placeholder="メールアドレスを入力"/>
+    <div class="px-4 pt-4">
+        <x-ui.search-bar />
+    </div>
+
 @endsection
 
