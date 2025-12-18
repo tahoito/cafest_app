0ec3b297a73cb288fdb6ba55df7b16892f7698cf
izumism8774 <haru.izumism.biz@gmail.com>
2025-12-18 14:49:07 +0900

    signup page

---
+++ src/resources/views/pages/store/auth/signup.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+@section('title','ログイン')
+
+@section('content')
+<div class="min-h-screen flex items-center justify-center bg-base relative overflow-hidden">
+    <div class="w-full max-w-md px-4">
+
+        <div class="mb-8 flex flex-col items-center">
+            <x-icons.store size="80" stroke="1" class="text-text mb-2" />
+            <h1 class="text-center text-text text-2xl">
+                ログイン    
+            </h1>
+        </div>
+
+        <form method="POST" action="{{ route('user.login') }}">
+        @csrf
+
+        <div class="space-y-6">
+            {{-- email --}}
+            <div class="space-y-1">
+            <x-ui.label for="email">メールアドレス</x-ui.label>
+            <x-ui.input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="メールアドレスを入力"
+                required
+                autocomplete="off"
+            />
+            </div>
+
+            {{-- password --}}
+            <div class="space-y-1">
+            <x-ui.label for="password">パスワード</x-ui.label>
+            <x-ui.input
+                id="password"
+                type="password"
+                name="password"
+                placeholder="パスワードを入力"
+                required
+                autocomplete="new-password"
+            />
+            </div>
+
+            {{-- password --}}
+            <div class="space-y-1">
+            <x-ui.label for="password">パスワード（確認）</x-ui.label>
+            <x-ui.input
+                id="password"
+                type="password"
+                name="password"
+                placeholder="パスワードを入力"
+                required
+                autocomplete="new-password"
+            />
+            </div>
+
+            {{-- button --}}
+            <div class="flex justify-center pt-4">
+            <x-ui.button type="submit" class="w-full text-main2">
+                サインアップ
+            </x-ui.button>
+            </div>
+        </div>
+        </form>
+
+        <div class="mt-4 text-center text-sm text-text">
+            ログインの方は
+            <a href="#" class="text-main2 hover:underline">    
+                こちら
+            </a>
+        </div>
+    </div>
+</div>
