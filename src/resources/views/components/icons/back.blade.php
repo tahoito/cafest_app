bde5b49f6ee7e04a73c6aed3da60725327684090
izumism8774 <haru.izumism.biz@gmail.com>
2025-12-18 13:59:41 +0900

    :wq wxit

---
+++ src/resources/views/components/icons/back.blade.php
@@ -0,0 +1,12 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 30 30"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  {{ $attributes->merge(['class' => 'inline-block']) }}
+>
+<path d="M12 19L5 12M5 12L12 5M5 12H19" stroke="#201200" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
