d9a8f3b18919f414a5d38dc8247fbbd518ad99b7
tahoito <taho.ito0304@gmail.com>
2025-12-25 09:41:00 +0900

    トップページ編集開始

--- src/resources/views/pages/user/top.blade.php
+++ src/resources/views/pages/user/top.blade.php
@@ -2,13 +2,29 @@
 @section('title','トップ')
 
 @section('content')
-<div class="min-h-screen flex items-center justify-center bg-base_color">
-	<div class="text-center">
-		<h1 class="text-2xl">ようこそ、トップページです</h1>
-		<p class="mt-4">ログインに成功しました。</p>
-		<div class="mt-6">
-			<a href="{{ route('welcome') }}" class="text-main hover:underline">ホームへ戻る</a>
-		</div>
-	</div>
+<div class="min-h-screen bg-base_color">
+    <div class="w-full max-w-md mx-auto pt-6 space-y-10">
+
+        <section class="px-4">
+            <x-ui.search-bar />
+        </section>
+
+        <section class="px-4 space-y-3">
+            <div class="text-lg text-text font-medium">おすすめのタグ</div>
+        </section>
+
+        <section class="px-4 space-y-3">
+            <div class="text-lg text-text font-medium">おすすめのカフェ</div>
+        </section>
+
+        <section class="px-4 space-y-3">
+            <div class="text-lg text-text font-medium">みんなのレビュー</div>
+        </section>
+
+        <section class="px-4 space-y-3 pb-24">
+            <div class="text-lg text-text font-medium">カフェ一覧</div>
+        </section>
+
+    </div>
 </div>
 @endsection
