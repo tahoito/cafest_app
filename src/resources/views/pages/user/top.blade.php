f3a69e35dc7df50dd84ad21a917d75a612980055
tahoito <taho.ito0304@gmail.com>
2025-12-25 09:49:39 +0900

    おすすめのタグ追加

--- src/resources/views/pages/user/top.blade.php
+++ src/resources/views/pages/user/top.blade.php
@@ -3,14 +3,21 @@
 
 @section('content')
 <div class="min-h-screen bg-base_color">
-    <div class="w-full max-w-md mx-auto pt-6 space-y-10">
+    <div class="w-full max-w-md mx-auto pt-6 space-y-5">
 
         <section class="px-4">
             <x-ui.search-bar />
         </section>
 
-        <section class="px-4 space-y-3">
+        <section class="px-4 space-y-2">
             <div class="text-lg text-text font-medium">おすすめのタグ</div>
+			<div class="-mx-4 px-4 flex gap-2 overflow-x-auto no-scrollbar">
+				<x-ui.tag active>映え</x-ui.tag>
+				<x-ui.tag>映え</x-ui.tag>
+				<x-ui.tag>映え</x-ui.tag>
+				<x-ui.tag>映え</x-ui.tag>
+				<x-ui.tag>映え</x-ui.tag>
+			</div>
         </section>
 
         <section class="px-4 space-y-3">
