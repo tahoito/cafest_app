42ebb9564a68c07ab59caa5bbbdf02d67644c140
izumism8774 <haru.izumism.biz@gmail.com>
2025-12-29 07:23:44 +0900

    店舗カード

--- src/resources/views/pages/test.blade.php
+++ src/resources/views/pages/test.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <x-ui.button class="w-[90%] mx-auto">ユーザーログイン</x-ui.button>
-    <x-ui.button theme="store" class="w-[90%] mx-auto">サインアップ</x-ui.button>
-    <x-ui.button variant="secondary" class="w-[70%] mx-auto">次へ</x-ui.button>
-    <x-ui.button variant="next">戻る</x-ui.button>
-    <x-ui.input type="text" placeholder="メールアドレスを入力"/>
-    <div class="px-4 pt-4">
-        <x-ui.search-bar />
-    </div>
-    <x-ui.chip variant="mood">韓国風</x-ui.chip>
-    <x-ui.chip variant="area">栄</x-ui.chip>
 
+@php
+$store = [
+'id' => 1,
+'name' => 'wiik coffee',
+'area' => '栄',
+'rating' => 4.2,
+'mood' => '韓国風',
+'image_url' => null,
+];
+@endphp
+
+<x-ui.store-card :store="$store" variant="list" />
 @endsection
-
