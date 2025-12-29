52ef07e5372750393e4b0a75eedc5278ab798257
tahoito <taho.ito0304@gmail.com>
2025-12-29 13:27:00 +0900

    文字の調節

--- src/resources/views/components/ui/search-tag.blade.php
+++ src/resources/views/components/ui/search-tag.blade.php
@@ -5,7 +5,7 @@
 ])
 
 @php
-$common = 'inline-flex items-center justify-center whitespace-nowrap rounded-full border px-[16px] py-[2px] text-sm transition select-none';
+$common = 'inline-flex items-center justify-center whitespace-nowrap rounded-full border px-[15px] py-[1px] text-sm transition select-none';
 $state = $disabled
   ? 'opacity-50 cursor-not-allowed'
   : 'cursor-pointer hover:opacity-90 active:scale-[0.98]';
