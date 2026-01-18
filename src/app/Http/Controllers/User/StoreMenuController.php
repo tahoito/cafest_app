711218ee9dee3b36d395a03cbcc38b694c676eab
tahoito <taho.ito0304@gmail.com>
2026-01-18 22:03:16 +0900

    ハート

--- src/app/Http/Controllers/User/StoreMenuController.php
+++ src/app/Http/Controllers/User/StoreMenuController.php
@@ -12,15 +12,14 @@ class StoreMenuController extends Controller
     {
         $userId = auth('user')->id();
 
-        $faved = \DB::table('user_favorites')
-            ->where('user_id', $userId)
-            ->where('store_id', $store->id)
-            ->exists();
+        $faved = auth()->check()
+            ? auth()->user()->favorites()->where('store_id', $store->id)->exists()
+            : false;
 
         $menuPhotos = $store->menuPhotos()->take(3)->get();
         $recommendedItems = $store->recommendedItems()->take(3)->get();
 
-        return view('pages.user.stores.menu', compact('store', 'menuPhotos', 'recommendedItems'));
+        return view('pages.user.stores.menu', compact('store', 'menuPhotos', 'recommendedItems','faved'));
     }
 
 
--- src/app/Http/Controllers/User/StorePostController.php
+++ src/app/Http/Controllers/User/StorePostController.php
@@ -16,13 +16,14 @@ class StorePostController extends Controller
 {
     public function index($storeId)
     {
+
+        $store = Store::findOrFail($storeId);
         $userId = auth('user')->id();
 
         $faved = auth()->check()
             ? auth()->user()->favorites()->where('store_id', $store->id)->exists()
             : false;
 
-        $store = Store::findOrFail($storeId);
 
         $posts = Review::with(['user','images'])
             ->where('store_id',$storeId)
@@ -44,7 +45,7 @@ class StorePostController extends Controller
                 });
             });
         
-        return view('pages.user.stores.posts', compact('store', 'posts'));
+        return view('pages.user.stores.posts', compact('store', 'posts','faved'));
     
     }
 
