057ab3e17d7ee6ef0ec3c485d2df05aff48bd7d7
tahoito <taho.ito0304@gmail.com>
2026-02-09 19:50:46 +0900

    edit

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders() // ← これを追加
+    ->withProviders() // ← ★これが必要
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
