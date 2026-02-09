6241c22ccf2ce85c001cffaa3948f569f787a31f
tahoito <taho.ito0304@gmail.com>
2026-02-09 21:50:55 +0900

    docker

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -4,9 +4,16 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="theme-color" content="#8A735A">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="default">
+  <meta name="apple-mobile-web-app-title" content="Cafest">
+
 
   <title>@yield('title', 'cafest')</title>
-
+  <link rel="manifest" href="/manifest.webmanifest" crossorigin="use-credentials">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+  <link rel="icon" href="/favicon.ico">
   <link rel="preconnect" href="https://fonts.bunny.net">
   <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
@@ -18,20 +25,21 @@
   $authBg = request()->routeIs('user.login', 'user.signup', 'store.login', 'store.signup');
 @endphp
 
-<body class="min-h-screen bg-base_color text-text_color relative overflow-x-hidden">
+<body class="min-h-screen bg-base_color text-text_color relative overflow-x-hidden isolate">
   @if($welcomeBg)
-    <div class="absolute top-[15px] -left-[80px] w-[180px] h-[180px] rounded-full bg-accent"></div>
-    <div class="absolute top-[30px] left-[120px] -translate-x-1/2 w-[36px] h-[36px] rounded-full bg-accent"></div>
-    <div class="absolute top-[120px] -right-[50px] w-[150px] h-[150px] rounded-full bg-accent"></div>
-    <div class="absolute -bottom-[120px] -left-[150px] w-[280px] h-[280px] rounded-full bg-accent"></div>
-    <div class="absolute bottom-[80px] left-[150px] -translate-x-1/2 w-[44px] h-[44px] rounded-full bg-accent"></div>
+    <div class="absolute top-[15px] -left-[80px] w-[180px] h-[180px] rounded-full bg-accent pointer-events-none -z-10"></div>
+    <div class="absolute top-[30px] left-[120px] -translate-x-1/2 w-[36px] h-[36px] rounded-full bg-accent pointer-events-none -z-10"></div>
+    <div class="absolute top-[120px] -right-[50px] w-[150px] h-[150px] rounded-full bg-accent pointer-events-none -z-10"></div>
+    <div class="absolute -bottom-[120px] -left-[150px] w-[280px] h-[280px] rounded-full bg-accent pointer-events-none -z-10"></div>
+    <div class="absolute bottom-[80px] left-[150px] -translate-x-1/2 w-[44px] h-[44px] rounded-full bg-accent pointer-events-none -z-10"></div>
   @elseif($authBg)
-    <div class="fixed inset-0 overflow-hidden pointer-events-none">
+    <div class="fixed inset-0 overflow-hidden pointer-events-none -z-10">
       <div class="absolute -top-24 -left-24 w-56 h-56 rounded-full bg-accent"></div>
       <div class="absolute -bottom-32 -right-20 w-64 h-64 rounded-full bg-accent"></div>
       <div class="absolute bottom-10 left-40 w-10 h-10 rounded-full bg-accent"></div>
     </div>
   @endif
+
 
   <main
     class="relative z-10 @unless(View::hasSection('hideNavbar')) pb-20 @else pb-2 @endunless"
